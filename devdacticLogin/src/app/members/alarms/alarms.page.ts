d4ae9ea8e3288fe2521bae98ef0f2ad41cc3cf67
Camboinho <gabrielcamboim99@gmail.com>
2020-02-02 20:18:12 -0300

    commit

--- devdacticLogin/src/app/members/alarms/alarms.page.ts
+++ devdacticLogin/src/app/members/alarms/alarms.page.ts
@@ -1,23 +1,127 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthenticationService } from "./../../services/authentication.service";
+import axios from "axios";
+import { NavController, Events } from "@ionic/angular";
+
 @Component({
   selector: "app-alarms",
   templateUrl: "./alarms.page.html",
   styleUrls: ["./alarms.page.scss"]
 })
 export class AlarmsPage implements OnInit {
-  constructor(private authService: AuthenticationService) {}
+  alarms = [{}];
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthenticationService,
+    private event: Events
+  ) {
+    this.event.subscribe("functionCall:tabSelected", eventData => {
+      this.authService.getCurrentUser().then(id => {
+        axios.defaults.headers.common = {
+          Authorization: "Bearer " + id.token
+        };
+
+        axios
+          .get("http://localhost:3000/users/" + id.user._id + "/alarms/")
+          .then(response => {
+            console.log(response.data);
+
+            this.alarms = response.data;
+
+            console.log(this.alarms);
+          });
+      });
+    });
+  }
+
+  ngOnInit() {
+    this.authService.getCurrentUser().then(id => {
+      axios.defaults.headers.common = {
+        Authorization: "Bearer " + id.token
+      };
+
+      axios
+        .get("http://localhost:3000/users/" + id.user._id + "/alarms/")
+        .then(response => {
+          console.log(response.data);
+
+          this.alarms = response.data;
+
+          console.log(this.alarms);
+        });
+    });
+  }
   logout() {
     this.authService.logout();
   }
+
   doRefresh(event) {
     console.log("Begin async operation");
+    this.authService.getCurrentUser().then(id => {
+      axios.defaults.headers.common = {
+        Authorization: "Bearer " + id.token
+      };
+
+      axios
+        .get("http://localhost:3000/users/" + id.user._id + "/alarms/")
+        .then(response => {
+          console.log(response.data);
+
+          this.alarms = response.data;
+
+          console.log(this.alarms);
+        });
+    });
 
     setTimeout(() => {
       console.log("Async operation has ended");
       event.target.complete();
     }, 2000);
+  }
+
+  deletarAlarme(idAlarme) {
+    this.authService.getCurrentUser().then(id => {
+      axios.defaults.headers.common = {
+        Authorization: "Bearer " + id.token
+      };
+
+      axios
+        .post("http://localhost:3000/users/alarms/" + idAlarme + "/delete")
+        .then(e =>
+          axios
+            .get("http://localhost:3000/users/" + id.user._id + "/alarms/")
+            .then(response => {
+              console.log("pegou");
+
+              this.alarms = response.data;
+
+              console.log(e);
+            })
+        )
+        .catch(e => console.log(e));
+    });
+  }
+
+  mudarEstado(idAlarme, estado) {
+    this.authService.getCurrentUser().then(id => {
+      axios.defaults.headers.common = {
+        Authorization: "Bearer " + id.token
+      };
+
+      axios
+        .post("http://localhost:3000/users/alarms/" + idAlarme + "/" + estado)
+        .then(e =>
+          axios
+            .get("http://localhost:3000/users/" + id.user._id + "/alarms/")
+            .then(response => {
+              console.log("pegou");
+
+              this.alarms = response.data;
+
+              console.log(e);
+            })
+        )
+        .catch(e => console.log(e));
+    });
   }
 }
--- devdacticLogin/src/app/members/criaralarme/criaralarme.page.ts
+++ devdacticLogin/src/app/members/criaralarme/criaralarme.page.ts
@@ -13,21 +13,22 @@ export class CriaralarmePage implements OnInit {
   devices = [{}];
   nomeAlarme: String;
   alarme;
-  hora: number;
-  minuto: number;
-  repetir;
 
-  dias = [
-    {
-      dom: "false",
-      seg: "false",
-      ter: "false",
-      qua: "false",
-      qui: "false",
-      sex: "false",
-      sab: "false"
-    }
-  ];
+  hora;
+  minuto;
+
+  repetir = false;
+  cen = [];
+
+  dias = {
+    dom: false,
+    seg: false,
+    ter: false,
+    qua: false,
+    qui: false,
+    sex: false,
+    sab: false
+  };
 
   constructor(
     private authService: AuthenticationService,
@@ -42,7 +43,7 @@ export class CriaralarmePage implements OnInit {
       };
 
       axios
-        .get("http://18.231.176.98:3000/users/" + id.user._id + "/devices/")
+        .get("http://localhost:3000/users/" + id.user._id + "/devices/")
         .then(response => {
           this.devices = response.data.map(elem => ({
             ...elem,
@@ -78,6 +79,50 @@ export class CriaralarmePage implements OnInit {
   }
 
   criar() {
+    for (var v = 0; v < this.devices.length; v++) {
+      //@ts-ignore
+      if (this.devices[v].checked === true) {
+        this.cen.push({
+          //@ts-ignore
+          deviceID: this.devices[v]._id,
+          estado_desejado: "ligado"
+        });
+      } else {
+        this.cen.push({
+          //@ts-ignore
+          deviceID: this.devices[v]._id,
+          estado_desejado: "desligado"
+        });
+      }
+    }
+
+    this.alarme = {
+      repetir: this.repetir,
+      horario: { hora: this.hora, minuto: this.minuto },
+      dias: this.dias,
+      cena: this.cen
+    };
+
+    console.log("OPAAAAAA");
+    console.log(this.alarme);
+
+    this.authService.getCurrentUser().then(id => {
+      axios.defaults.headers.common = {
+        Authorization: "Bearer " + id.token
+      };
+
+      axios
+        .post(
+          "http://localhost:3000/users/" +
+            id.user._id +
+            "/alarms/create/" +
+            this.nomeAlarme,
+          this.alarme
+        )
+        .then(e => console.log(e))
+        .catch(e => console.log(e));
+    });
+
     this.tabSelected();
     this.location.back();
   }
